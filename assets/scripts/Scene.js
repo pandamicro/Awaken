3f78245999dd8e312f109249e444ca87d09ec93d
pandamicro <lphuabin@gmail.com>
2016-06-25 21:37:49 +0800

    Add light

--- assets/scripts/Scene.js
+++ assets/scripts/Scene.js
@@ -1,11 +1,39 @@
+var SceneConfig = require('SceneConfig');
+var VisibilityPolygon = require('visibility_polygon_dev');
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        level: 0,
+        light: cc.Node,
+        lightColor: cc.Color
     },
 
     // use this for initialization
     onLoad: function () {
+        var polygons = SceneConfig[this.level];
+        var segments = VisibilityPolygon.convertToSegments(polygons);
+        segments = VisibilityPolygon.breakIntersections(segments);
 
+        var width = this.node.width;
+        var height = this.node.height;
+        segments.unshift([[-1,-1],[width+1,-1],[width+1,height+1],[-1,height+1]]);
+
+        // var lightTrans = this.light.getNodeToWorldTransform();
+        var position = [this.light.x - this.node.x + width/2, this.light.y - this.node.y + height/2];
+        var vertices = VisibilityPolygon.computeViewport(position, segments, [-this.node.x, -this.node.y], [width, height]);
+
+        var lineColor = cc.color(0, 0, 0, 0);
+
+        var drawNode = new cc.DrawNode();
+        this.node._sgNode.addChild(drawNode, 10);
+        drawNode.x = -width/2;
+        drawNode.y = -height/2;
+        drawNode.clear();
+        for (var i = 0, l = vertices.length; i < l; ++i) {
+            vertices[i] = cc.p(vertices[i][0], vertices[i][1]);
+        }
+        drawNode.drawPoly(vertices, this.lightColor, 5, lineColor);
     }
 });
--- tilemap-parser.js
+++ tilemap-parser.js
@@ -3,6 +3,7 @@ var fs = require('fs') || null;
 
 var i = 2;
 var url;
+var width = 1136;
 var height = 640;
 while ( i < process.argv.length) {
     var arg = process.argv[i];
@@ -11,6 +12,11 @@ while ( i < process.argv.length) {
     case '--url' :
     case '-u' :
         url = process.argv[i+1];
+        i += 2;
+        break;
+    case '--width':
+    case '-w' :
+        width = parseInt(process.argv[i+1]);
         i += 2;
         break;
     case '--height':
@@ -31,15 +37,15 @@ if (url) {
         var polys = result.map.objectgroup[0].object;
         for (i = 0; i < polys.length; ++i) {
             var poly = polys[i];
-            var x = parseFloat(poly['$'].x);
-            var y = parseFloat(poly['$'].y);
+            var x = parseInt(poly['$'].x);
+            var y = parseInt(poly['$'].y);
             var pointstr = poly.polygon[0]['$'].points;
             var pts = pointstr.split(' ');
             for (var j = 0; j < pts.length; ++j) {
                 pts[j] = pts[j].split(',');
                 pts[j] = [
-                    parseFloat(pts[j][0]) + x,
-                    height - (parseFloat(pts[j][1]) + y)
+                    parseInt(pts[j][0]) + x,
+                    height - (parseInt(pts[j][1]) + y)
                 ];
             }
             polygons.push(pts);
