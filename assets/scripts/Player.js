ee848b8baf982602c43855f5c174323ba9fbb0b2
pandamicro <lphuabin@gmail.com>
2016-06-26 13:48:02 +0800

    Fix die with flash, add camera animation to level 2

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -68,8 +68,8 @@ var Player = cc.Class({
     lightUp: function () {
         var succeed = this.scene.lightUp();
 
-        this.enabled = false;
         if (succeed) {
+            this._active = false;
             this.scene.startSpread();
         }
         else {
